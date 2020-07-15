363de37a47227f18c012dd11a18a72bb08e65219
DEVEZEAUD PHILIPPE <pdevezeaud@yahoo.fr>
2020-07-15 14:14:43 +0200

    Les conditions

---
+++ ifElse.js
@@ -0,0 +1,30 @@
+let heure = 19;
+
+// heure == 11
+// heure != 11
+// heure < 11
+// heure > 11
+// heure <= 11
+// heure >= 11
+
+if (heure <= 12) {
+    console.log("c'est midi.");
+}else if (heure <18)
+{
+    console.log("C'est l'après midi");
+}else
+{
+    console.log("c'est le soir!!");
+}
+
+
+let age = 21;
+
+if (age < 18){
+    console.log("Vous n'etes pas majeur.")
+
+}else if (age <= 20){
+    console.log("Vous etes majeur en France.")
+}else {
+    console.log("Vous etes majeur partout, à vous les casinos.");
+}
