d1f9188146ecfa02006d8c8659b166857ed00e0d
DEVEZEAUD PHILIPPE <pdevezeaud@yahoo.fr>
2020-07-19 20:35:30 +0200

    TP CALCULETTE

---
+++ test.js
@@ -0,0 +1,10 @@
+let age = 18, statut = "étudiant";
+if(age < 18 || statut == "étudiant") {
+    console.log('Partie 1');
+}
+else if(age == 18 && statut != "étudiant") {
+    console.log('Partie 2');
+}
+else {
+    console.log('Partie 3');
+}
