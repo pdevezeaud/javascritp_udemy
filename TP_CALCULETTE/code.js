24b7ac05e62bd9ab3c1a1a6dfce3bd255c505b4d
DEVEZEAUD PHILIPPE <pdevezeaud@yahoo.fr>
2020-07-20 18:18:56 +0200

    section 50 & 51 TP calculette 2 (oubli)

--- TP_CALCULETTE/code.js
+++ TP_CALCULETTE/code.js
@@ -4,26 +4,26 @@ do {
 
     choix = prompt("Que souhaitez vous faire\n\n 1 - Addition\n  2 - Multiplication\n  3 - Soustraction\n  4 - Division\n");
 
-}while (choix == "" || choix == null)
+} while (choix == "" || choix == null)
 
 switch (choix) {
     case "1":
         console.log("Je fais une addition");
-    break;
+        break;
 
     case "2":
         console.log("Je fais une Multiplication");
-    break;
+        break;
 
     case "3":
         console.log("Soustraction");
-    break;
+        break;
 
     case "4":
         console.log("Je fais une Division");
-    break;
-        
-        
+        break;
+
+
 
     default:
         console.log("les choix autorisée : 1 à 4 !!!")
