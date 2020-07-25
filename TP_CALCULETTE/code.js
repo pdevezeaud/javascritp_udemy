de0c9aa9b799d27383b638edebef6116a261cbaf
DEVEZEAUD PHILIPPE <pdevezeaud@yahoo.fr>
2020-07-25 19:14:36 +0200

    ajout funciton addition Ok

--- TP_CALCULETTE/code.js
+++ TP_CALCULETTE/code.js
@@ -1,7 +1,7 @@
-
-
 let premierNombre;
-let secondNombre;
+let deuxiemeNombre;
+
+
 
 do {
 
@@ -9,33 +9,86 @@ do {
 
 } while (choix == "" || choix == null)
 
+
 switch (choix) {
+
     case "1":
         do {
-    
+
             premierNombre = prompt("Entrer le premier nombre : ");
             premierNombre = Number(premierNombre);
             deuxiemeNombre = prompt("Entrez le second nombre : ");
             deuxiemeNombre = Number(deuxiemeNombre);
             
-        } while ( isNaN(premierNombre,deuxiemeNombre));
+        } while (isNaN(premierNombre, deuxiemeNombre));
+        
+        function addition(nombreA, nombreB)
+        {
+            let resultat = nombreA + nombreB;
+            return resultat;
+           
+        }
+
+        console.log(addition(premierNombre,deuxiemeNombre));
+
         break;
+        
+        
+        case "2":
+            do {
+                
+                premierNombre = prompt("Entrer le premier nombre : ");
+                nombreA = Number(premierNombre);
+                deuxiemeNombre = prompt("Entrez le second nombre : ");
+                deuxiemeNombre = Number(deuxiemeNombre);
+                
+            } while (isNaN(premierNombre, deuxiemeNombre));
+            
+            function multiplication(nombreA, nombreB){
+                
+                let resultatMultiplication = nombreA * nombreB;
+                return resultatmultiplication;
+            }
+            break;
+            
+            case "3":
+                do {
+                    
+                    premierNombre = prompt("Entrer le premier nombre : ");
+                    premierNombre = Number(premierNombre);
+                    deuxiemeNombre = prompt("Entrez le second nombre : ");
+                    deuxiemeNombre = Number(deuxiemeNombre);
+                    
+                } while (isNaN(premierNombre, deuxiemeNombre));
+                
+                function soustraction(nombreA, nombreB){
+                    
+                    let resultatSoustraction = nombreA - nombreB;
+                    return resultatSoustraction;
+                }
+                break;
+                
+                case "4":
+                    do {
+                        
+                        premierNombre = prompt("Entrer le premier nombre : ");
+                        let nombreA = Number(premierNombre);
+                        deuxiemeNombre = prompt("Entrez le second nombre : ");
+                        deuxiemeNombre = Number(deuxiemeNombre);
+                        
+                    } while (isNaN(premierNombre, deuxiemeNombre));
+                    
+                    function division(nombreA, nombreB){
+                        
+                        let resultatDivision = nombreA / nombreB;
+                        return resultatDivision;
+                    }
+                    break;
+                    
+                    default:
+                        console.log("les choix autorisée : 1 à 4 !!!")
+                        break;
+                    }
 
-    case "2":
-        console.log("Je fais une Multiplication");
-        break;
-
-    case "3":
-        console.log("Soustraction");
-        break;
-
-    case "4":
-        console.log("Je fais une Division");
-        break;
 
 
-
-    default:
-        console.log("les choix autorisée : 1 à 4 !!!")
-        break;
-}
