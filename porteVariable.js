575106d8afe71be8269208d8d4bdb40f4b7dc42f
DEVEZEAUD PHILIPPE <pdevezeaud@yahoo.fr>
2020-07-26 16:40:59 +0200

    exemple portee variable

---
+++ porteVariable.js
@@ -0,0 +1,16 @@
+let variableLet = "globale";
+var variableVar = "globale";
+
+if(true) {
+//on modifie la variable
+let variableLet = "locale";
+var variableVar = "locale";
+
+console.log("dans le if let " + variableLet);
+console.log("var dans le if var " + variableVar);
+
+}
+
+console.log("let " + variableLet);
+console.log("var " + variableVar);
+
