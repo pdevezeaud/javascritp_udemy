b2db49d8cd3e0a7c3223d46c72e0ceac16a49af9
DEVEZEAUD PHILIPPE <pdevezeaud@yahoo.fr>
2020-08-29 22:11:51 +0200

    Debut du juste prix

--- BAC_A_SABLE/bac_a_sable.js
+++ BAC_A_SABLE/bac_a_sable.js
@@ -1,11 +1,54 @@
 
-let logo = document.querySelector('img');
+// let logo = document.querySelector('img');
 
 
-function conversion() {
-    let somme = prompt("Entrez la valeur à convertir en €uros :");
-    somme = Number(somme);
-    let resultat = somme * 6.55957;
-    alert(somme + " €\n" + resultat + " FR");
+// function conversion() {
+//     let somme = prompt("Entrez la valeur à convertir en €uros :");
+//     somme = Number(somme);
+//     let resultat = somme * 6.55957;
+//     alert(somme + " €\n" + resultat + " FR");
+// }
+// conversion();
+
+
+//TRAVAILLER SUR LES TABLEAUX
+const guests = [
+    'Will Alexander',
+    'Sarah Kate',
+    'Audrey Simon'
+];
+
+// //console.log(guests.length);
+// guests.push('Philippe Devezeaud');
+// console.log(guests);
+// //place en début de tableau
+// guests.unshift('Toto Mamassidiran');
+// console.log(guests);
+// //retir le dernier element
+// guests.pop();
+// console.log(guests);
+// console.log(guests.length);
+
+
+//un Set ou ensemble est une liste non ordonné.Les doublons ne
+//ne sont pas autorisés
+//un Map est une liste ordonnée de paires clés/valeurs
+
+// let number = 10;
+// number += 5;
+// console.log(number);
+// number /= 3;
+// console.log(number);
+// number ++;
+// console.log(number);
+
+
+// if else
+let userIsloggedIn = false;
+
+if(!userIsloggedIn) {
+    console.log("Utilisateur bien connecté");
+}else {
+    console.log("Utilisateur en faute");
 }
-conversion();
+
