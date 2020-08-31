457a5a8680115723e60f9fe9e11df80fa0dd261d
DEVEZEAUD PHILIPPE <pdevezeaud@yahoo.fr>
2020-08-31 18:52:36 +0200

    if else

--- citation/app.js
+++ citation/app.js
@@ -142,15 +142,57 @@
 
 //CREATION D'OBJET PAR CLASSE (INSTANCIATION)
 
-class book {
-    constructor(newTitle,newAuthor,newPages){
-        this.title = newTitle;
-        this.author = newAuthor;
-        this.pages = newPages;
+// class book {
+//     constructor(newTitle,newAuthor,newPages){
+//         this.title = newTitle;
+//         this.author = newAuthor;
+//         this.pages = newPages;
+//     }
+// }
+
+// let monLibre = new book("L'histoire de Tao", "Will Alexander",250);
+// let MaFeuilleDeChou = new book("L'histoire de Philippe", "Will Jamilloux",25);
+
+// console.log(monLibre);
+
+//STRUCTURE IF/ELSE
+
+class Episode {
+    constructor(title, duration, minutesWatched) {
+      this.title = title;
+      this.duration = duration;
+      
+      
+      // Add conditions here
+      // =================================
+      
+      if (duration == minutesWatched) {
+        this.hasBeenWatched = true;
+      } else if (duration < minutesWatched) {
+        this.hasBeenWatched = false;
+      }
+      // =================================
     }
-}
-
-let monLibre = new book("L'histoire de Tao", "Will Alexander",250);
-let MaFeuilleDeChou = new book("L'histoire de Philippe", "Will Jamilloux",25);
-
-console.log(monLibre);
+  }
+  
+  let firstEpisode = new Episode('Dark Beginnings', 45, 45);
+  let secondEpisode = new Episode('The Mystery Continues', 45, 10);
+  let thirdEpisode = new Episode('An Unexpected Climax', 60, 0);
+  
+  let episodes = [firstEpisode, secondEpisode, thirdEpisode];
+  
+  const body = document.querySelector('body');
+  
+  for(let i = 0; i < 3; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('series-frame');
+    let newTitle = document.createElement('h2');
+    newTitle.innerText = 'The Story of Tau';
+    let newParagraph = document.createElement('p');
+    newParagraph.innerText = `${episodes[i].title}
+  ${episodes[i].duration} minutes
+  ${episodes[i].hasBeenWatched ? 'Already been watched' : 'Not yet watched'}`;
+    newDiv.append(newTitle);
+    newDiv.append(newParagraph);
+    body.append(newDiv);
+  }
