00c15a9833272e03b4fd34697bac9db7395e8437
Philippe Devezeaud <A143754@dekra-automotivesolutions.com>
2021-08-13 14:39:35 +0200

    application Meteo Fin

--- LesApi_Post/main.js
+++ LesApi_Post/main.js
@@ -1,13 +1,20 @@
-const url = 'https://lesoublisdelinfo.com/api.php';
+//const url = 'https://api.openweathermap.org/data/2.5/weather?q=saint-saulve&appid=?a7da75044719ccd7ca2ee4780cb08ab0&units=metric';  //pour get
+const url = 'https://lesoublisdelinfo.com/api.php?a7da75044719ccd7ca2ee4780cb08ab0';  //pour POST
 
 let requete = new XMLHttpRequest();
 
 //GET
 requete.open("GET", url);
-
 requete.responseType = 'json';
-
 requete.send();
+
+//POST
+requete.open('POST', url);
+// on envoi à l'api
+requete.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+requete.responseType = 'json';
+requete.send('prenom=John');
+
 
 requete.onload = function() {
     if (requete.readyState === XMLHttpRequest.DONE) {
