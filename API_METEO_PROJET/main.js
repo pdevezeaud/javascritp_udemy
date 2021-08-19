81a2e9bce37adcbf4ac470dc0df37a63315f7728
Philippe Devezeaud <A143754@dekra-automotivesolutions.com>
2021-08-19 15:11:07 +0200

    api_meteo modif

--- API_METEO_PROJET/main.js
+++ API_METEO_PROJET/main.js
@@ -10,7 +10,7 @@ changerVille.addEventListener('click', () => {
 
 
 function recevoirTemperature(ville){
-
+    
     const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=a7da75044719ccd7ca2ee4780cb08ab0&units=metric';
 
 
@@ -41,4 +41,3 @@ function recevoirTemperature(ville){
 }
 
 
-//setInterval(recevoirTemperature, 5000);
