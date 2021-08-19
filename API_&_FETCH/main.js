6830fac8b73a0af507088efeb8d117cb7c494f1e
DEVEZEAUD PHILIPPE <pdevezeaud@yahoo.fr>
2021-08-19 18:24:21 +0200

    correction bug

--- API_&_FETCH/main.js
+++ API_&_FETCH/main.js
@@ -3,7 +3,7 @@
 //on ecoute l'evenement sur ID cp et l'input.
 document.querySelector('#cp').addEventListener('input', function () {
     //on test si il entre 5 carateres 
-    if(this.value.lenght == 5) {
+    if(this.value.length == 5) {
         let url = `https://geo.api.gouv.fr/communes?codePostal=${this.value}&fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centre`;
 
         fetch(url).then((response) => 
