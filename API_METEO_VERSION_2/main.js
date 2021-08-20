6daf03b386e52489a4729265500fedaac39cf6f2
DEVEZEAUD PHILIPPE <pdevezeaud@yahoo.fr>
2021-08-20 19:43:25 +0200

    ajout des brique + url

--- API_METEO_VERSION_2/main.js
+++ API_METEO_VERSION_2/main.js
@@ -1,1 +1,11 @@
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=a7da75044719ccd7ca2ee4780cb08ab0&units=metric';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=cognac&appid=a7da75044719ccd7ca2ee4780cb08ab0&units=metric&lang=fr';
+
+fetch(url).then((response ) => 
+response.json().then((data) => {
+    console.log(data);
+    document.querySelector('#city').innerHTML=data.name;
+    document.querySelector('#temp').innerHTML=data.main.temp;
+    document.querySelector('#humidity').innerHTML=data.main.humidity + '%';
+    document.querySelector('#wind').innerHTML=data.wind.speed + 'Km/h';
+})
+);
