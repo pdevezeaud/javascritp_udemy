cfd6deaa03cd7d0fd580e1cd8b2c39fc134de2bb
Philippe Devezeaud <A143754@dekra-automotivesolutions.com>
2021-08-20 10:35:05 +0200

    ajout de l'affichage HTML

--- API_&_FETCH/main.js
+++ API_&_FETCH/main.js
@@ -3,11 +3,26 @@
 //on ecoute l'evenement sur ID cp et l'input.
 document.querySelector('#cp').addEventListener('input', function () {
     //on test si il entre 5 carateres 
-    if(this.value.lenght == 5) {
+    if (this.value.length == 5) {
         let url = `https://geo.api.gouv.fr/communes?codePostal=${this.value}&fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centre`;
 
-        fetch(url).then((response) => 
-        response.json().then((data) => console.log(data))
-        );
+        fetch(url).then((reponse) =>
+            reponse.json().then((data) => {
+                console.log(data);
+                let affichageUl = '<ul>';
+                //boucle pour recuperer toute les villes dans un <li>
+                for (const ville of data) {
+                
+                    //on ajoute a affiche un  <li> dans le <ul>
+                    affichageUl += `<li><strong>${ville.nom}</strong> - ${ville.population} habitants.</li>`
+                } 
+                affichageUl +='</ul'; // on ferme le ul de l'affichageUl
+
+                // on selection id ville pour afficher dans le HTML avec le innerhtml
+                document.querySelector('#ville').innerHTML = affichageUl;
+
+            })
+        ).catch(error => console.log('Erreur : ' + error))
+    
     }
 });
