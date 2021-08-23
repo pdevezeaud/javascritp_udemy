692920c679a3e1e546ad83138dce4b91cf28de46
Philippe <pdevezeaud@yahoo.fr>
2021-08-23 13:52:05 +0200

    ajout fonction apicall

--- API_METEO_VERSION_2/main.js
+++ API_METEO_VERSION_2/main.js
@@ -1,11 +1,31 @@
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=cognac&appid=a7da75044719ccd7ca2ee4780cb08ab0&units=metric&lang=fr';
 
-fetch(url).then((response ) => 
+/**Appel a l'API openweather avec ville en parametre de la fonction */
+
+let apiCall = function(city) {
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a7da75044719ccd7ca2ee4780cb08ab0&units=metric&lang=fr`;
+
+    fetch(url).then((response ) => 
 response.json().then((data) => {
-    console.log(data);
+   
     document.querySelector('#city').innerHTML=data.name;
-    document.querySelector('#temp').innerHTML=data.main.temp;
-    document.querySelector('#humidity').innerHTML=data.main.humidity + '%';
-    document.querySelector('#wind').innerHTML=data.wind.speed + 'Km/h';
-})
-);
+    document.querySelector('#temp').innerHTML= "<i class='fas fa-thermometer-half'></i>" +data.main.temp;
+    document.querySelector('#humidity').innerHTML="<i class='fas fa-tint'></i>" + data.main.humidity + '%';
+    document.querySelector('#wind').innerHTML="<i class='fas fa-wind'></i>"+ data.wind.speed + 'Km/h';
+    })  
+)
+.catch((erreur) => console.log('erreur : ' + erreur));
+
+};
+
+/**Ecouteur d'evenement sur la soumission du fourmulaire */
+document.querySelector('form').addEventListener('submit', function(e){
+    // on casse l'element par défaut
+    e.preventDefault();
+
+    let ville = document.querySelector('#imputcity').value;
+
+    /**function apicall */
+    apiCall(ville);
+});
+
+apiCall('Rouen')
