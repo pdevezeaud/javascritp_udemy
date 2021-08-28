fd2cd40fb9c899921ed3a413bf0f0ed239e6d4c0
devezeaud philippe <pdevezeaud@yahoo.fr>
2021-08-28 14:38:47 +0200

    commit a vide

--- Callbacks_Promises_async-await/test_promessse.js
+++ Callbacks_Promises_async-await/test_promessse.js
@@ -1,80 +1,110 @@
-// methode asynchrome vieille methode
-let a;
-let b;
+// // methode asynchrome vieille methode
+// let a;
+// let b;
 
 
-// setTimeout(() => {
-//   console.log('set A')
-//   a = 1;
+// // setTimeout(() => {
+// //   console.log('set A')
+// //   a = 1;
+// //   setTimeout(() => {
+// //     console.log('set B')
+// //     b = 2;
+// //     let c = a + b;
+// //     console.log(c);
+// //   }, 2000);
+// // }, 3000);
+
+// // Avec les promesse
+// let p1 = new Promise(function(resolve,reject) {
+
 //   setTimeout(() => {
-//     console.log('set B')
-//     b = 2;
-//     let c = a + b;
-//     console.log(c);
-//   }, 2000);
-// }, 3000);
+//     console.log('promesse A');
+//     a = 2;
+//     //J'ai fini le traitement je renvoi le resultat
+//     resolve(a);  
+//   }, 1000);
+// });
 
-// Avec les promesse
-let p1 = new Promise(function(resolve,reject) {
+// p1.then(function(a){
 
-  setTimeout(() => {
-    console.log('promesse A');
-    a = 2;
-    //J'ai fini le traitement je renvoi le resultat
-    resolve(a);  
-  }, 1000);
-});
+//   console.log("after " + a);
 
-p1.then(function(a){
+//   let p2 = new Promise(function(resolve,reject) {
+//     setTimeout(() => {
+//       console.log('promesse B');
+//       b = 1;
+//       //J'ai fini le traitement je renvoi le resultat
+//       resolve(b);  
+//     }, 3000);
+//   });
+//   return p2
+// })
 
-  console.log("after " + a);
+// .then(function(){
+//   let c = a + b;
+//   console.log('le resulat de A & B : '+ c)
+// });
 
-  let p2 = new Promise(function(resolve,reject) {
-    setTimeout(() => {
-      console.log('promesse B');
-      b = 1;
-      //J'ai fini le traitement je renvoi le resultat
-      resolve(b);  
-    }, 3000);
-  });
-  return p2
-})
 
-.then(function(){
-  let c = a + b;
-  console.log('le resulat de A & B : '+ c)
-});
+// /**
+//  * 
+//  * Possibilité de simplifié l'ecriture pour rendre plus lisible
+//  */
+
+//  let p1 = new Promise(function(resolve,reject) {
+
+//   setTimeout(() => {
+//     console.log('promesse A');
+//     a = 2;
+//     //J'ai fini le traitement je renvoi le resultat
+//     resolve(a);  
+//   }, 1000);
+// });
+
+// let p2 = new Promise(function(resolve,reject) {
+//   setTimeout(() => {
+//     console.log('promesse B');
+//     b = 1;
+//     //J'ai fini le traitement je renvoi le resultat
+//     resolve(b);  
+//   }, 3000);
+// });
+
+// // tableau pour rassembler les promesses
+// let arr = [p1,p2];
+
+// // La méthode all qui va attendre un tableau de promesse (arr)
+
+// Promise.all(arr).then(function(resultat){
+//   console.log(resultat);
+// })
 
 
 /**
  * 
- * Possibilité de simplifié l'ecriture pour rendre plus lisible
+ * async et await
+ * 
  */
 
- let p1 = new Promise(function(resolve,reject) {
+async function test(){
+  return await new Promise(
+    (resolve) => {
+      setTimeout(() => {
+          resolve('x')
+      }, 2000);
+    }
+  )
 
-  setTimeout(() => {
-    console.log('promesse A');
-    a = 2;
-    //J'ai fini le traitement je renvoi le resultat
-    resolve(a);  
-  }, 1000);
-});
+}
 
-let p2 = new Promise(function(resolve,reject) {
-  setTimeout(() => {
-    console.log('promesse B');
-    b = 1;
-    //J'ai fini le traitement je renvoi le resultat
-    resolve(b);  
-  }, 3000);
-});
+test().then(
+  (result) => {console.log(result)}
 
-// tableau pour rassembler les promesses
-let arr = [p1,p2];
+)
 
-// La méthode all qui va attendre un tableau de promesse (arr)
 
-Promise.all(arr).then(function(resultat){
-  console.log(resultat);
-})
+const fn = (hello) => {
+  console.log('hello');
+}
+
+fn('buenos dias');
